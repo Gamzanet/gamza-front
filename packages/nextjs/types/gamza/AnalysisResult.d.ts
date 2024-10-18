f46a0c3b41953797ea3f4c5fd7c29d5394048205
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2024-10-18 23:28:32 +0900

    ✨ Feat : mock response
    - define response schema
    - mock expected response

---
+++ packages/nextjs/types/gamza/AnalysisResult.d.ts
@@ -0,0 +1,30 @@
+export interface AnalysisResult {
+  hookGasConsumption: HookGasConsumption;
+  TokenPriceDifference: TokenPriceDifference;
+  testLogs: TestLog[];
+  // advancedAnalysis: AdvancedAnalysis | undefined;
+}
+
+export interface HookGasConsumption {
+  swap: number;
+  addLiquidity: number;
+  removeLiquidity: number;
+  donate: number;
+}
+
+export interface TokenPriceDifference {
+  expect: number;
+  actual: number;
+}
+
+export interface TestLog {
+  type: string;
+  title: string;
+  pass: boolean;
+  summary: string;
+  description: string;
+  trace: string;
+}
+
+// TODO: Implement AdvancedAnalysis
+// export interface AdvancedAnalysis {}
