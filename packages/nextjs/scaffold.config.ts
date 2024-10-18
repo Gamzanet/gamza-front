e262c5d96e3d6cac0e2c6aa2a1470f056e737edb
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2024-10-18 22:49:10 +0900

    🛠️ Chore : add unichain

--- packages/nextjs/scaffold.config.ts
+++ packages/nextjs/scaffold.config.ts
@@ -1,3 +1,4 @@
+import { defineChain } from "viem";
 import * as chains from "viem/chains";
 
 export type ScaffoldConfig = {
@@ -8,9 +9,41 @@ export type ScaffoldConfig = {
   onlyLocalBurnerWallet: boolean;
 };
 
+export const unichain = /*#__PURE__*/ defineChain({
+  id: 1301,
+  name: "Unichain",
+  nativeCurrency: { name: "Unichain Ether", symbol: "ETH", decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ["https://sepolia.unichain.org"],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "Etherscan",
+      url: "https://sepolia.uniscan.xyz/",
+      apiUrl: "https://api-sepolia.uniscan.xyz/api",
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: "0xca11bde05977b3631167028862be2a173976ca11",
+      blockCreated: 0,
+    },
+    // TODO Add the address of the Unichain's ENS registry
+    ensRegistry: { address: "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e" },
+    // TODO Add the address of the Unichain's ENS resolver
+    ensUniversalResolver: {
+      address: "0xc8Af999e38273D658BE1b921b88A9Ddf005769cC",
+      blockCreated: 5_317_080,
+    },
+  },
+  testnet: true,
+});
+
 const scaffoldConfig = {
   // The networks on which your DApp is live
-  targetNetworks: [chains.foundry],
+  targetNetworks: [chains.sepolia, unichain],
 
   // The interval at which your front-end polls the RPC servers for new data
   // it has no effect if you only target the local network (default is 4000)
