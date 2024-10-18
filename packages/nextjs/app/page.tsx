fb24d08132e08afd93e7b5b4ce980f6ddcf6616c
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2024-10-19 01:31:39 +0900

    🔄 Refactor : detach static content

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -3,15 +3,15 @@ import type { NextPage } from "next";
 import UserInputForm from "~~/components/gamza/UserInputForm";
 
 const Home: NextPage = () => {
+  const title = "Gamza Analyzer";
+  const description =
+    "Enter the PoolKey, validate its authenticity, and identify potential risks such as insufficient liquidity, unexpected behavior, or known vulnerabilities before proceeding.";
   return (
     <main className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left mx-2">
-          <h1 className="text-5xl font-bold">Gamza Analyzer</h1>
-          <p className="py-6">
-            Enter the PoolKey, validate its authenticity, and identify potential risks such as insufficient liquidity,
-            unexpected behavior, or known vulnerabilities before proceeding.
-          </p>
+          <h1 className="text-5xl font-bold">{title}</h1>
+          <p className="py-6">{description}</p>
           <BugAnt />
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
