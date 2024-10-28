c0c4824f5cd919580bd948946fac4319520c1472
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2024-10-29 07:43:57 +0900

    🔧 Modify : simple copy of legacy

---
+++ packages/nextjs/app/dev2/TestLogs.tsx
@@ -0,0 +1,16 @@
+import { FC } from "react";
+import LogBox from "~~/components/gamza/renewal/LogBox";
+import { TestLog } from "~~/types/gamza/AnalysisResult";
+
+const TestLogsScope: FC<{ props: TestLog[] }> = ({ props }) => {
+  return (
+    <div className="bg-base-200 m-2 space-y-2 p-4">
+      <header className="text-center text-3xl font-bold">TestLogs</header>
+      {props.map(log => (
+        <LogBox key={Math.random().toString(36)} data={log} />
+      ))}
+    </div>
+  );
+};
+
+export default TestLogsScope;
