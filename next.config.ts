b2444e9e1fe2445b68d35142ee41f38d8bac608c
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2024-10-29 22:41:49 +0900

    🛠️ Chore : init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,35 @@
+// @ts-check
 import type { NextConfig } from "next";
+import {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} from "next/constants";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = (
+  phase: string,
+  { defaultConfig }: { defaultConfig: NextConfig }
+): NextConfig => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      ...defaultConfig,
+      target: "server",
+      env: {
+        API_URL: "http://localhost:3000",
+      },
+    };
+  }
+
+  if (phase === PHASE_PRODUCTION_BUILD) {
+    return {
+      ...defaultConfig,
+      target: "server",
+      env: {
+        API_URL: "https://yourdomain.com",
+      },
+    };
+  }
+
+  return defaultConfig;
 };
 
 export default nextConfig;
